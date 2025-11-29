22f425280f7ab4c8e767aba629d8404fea494073
J-yriz <alfaziz2006@gmail.com>
2025-11-29 13:32:53 +0700

    feat: add new ENUM types for enrollment status, payment method, and payment status

--- backend/internal/database/enum.go
+++ backend/internal/database/enum.go
@@ -18,6 +18,9 @@ import (
 func CreateAllEnums(db *gorm.DB) {
 	// 1️⃣ Buat ENUM untuk role user (admin, mentor, student)
 	createUserRoleEnum(db)
+	createEnrollmentStatusEnum(db)
+	createPaymentMethodEnum(db)
+	createPaymentStatusEnum(db)
 
 	// 2️⃣ Log bahwa seluruh ENUM berhasil diinisialisasi
 	log.Println("[Success] All ENUMs have been created")
@@ -47,4 +50,43 @@ func createUserRoleEnum(db *gorm.DB) {
 	db.Exec(query)
 
 	log.Println("[Success] ENUM user_role is ready for use")
+}
+
+func createEnrollmentStatusEnum(db *gorm.DB) {
+	query := `
+	DO $$ BEGIN
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'enrollment_status') THEN
+			CREATE TYPE enrollment_status AS ENUM ('active', 'completed', 'cancelled');
+		END IF;
+	END $$;
+	`
+	db.Exec(query)
+
+	log.Println("[Success] ENUM enrollment_status is ready for use")
+}
+
+func createPaymentMethodEnum(db *gorm.DB) {
+	query := `
+	DO $$ BEGIN
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_method') THEN
+			CREATE TYPE payment_method AS ENUM ('credit_card', 'bank_transfer', 'ewallet');
+		END IF;
+	END $$;
+	`
+	db.Exec(query)
+
+	log.Println("[Success] ENUM payment_method is ready for use")
+}
+
+func createPaymentStatusEnum(db *gorm.DB) {
+	query := `
+	DO $$ BEGIN
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_status') THEN
+			CREATE TYPE payment_status AS ENUM ('pending', 'success', 'failed');
+		END IF;
+	END $$;
+	`
+	db.Exec(query)
+
+	log.Println("[Success] ENUM payment_status is ready for use")
 }
